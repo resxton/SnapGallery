0e0612c57ab59989a42c5f4fb25b44fd31f48835
Kirill Somov <gornomirov661@gmail.com>
2025-04-11 09:05:18 +0300

    Initial Commit

---
+++ SnapGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapGallery
+//
+//  Created by Сомов Кирилл on 11.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
